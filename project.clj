62e8b480fbb19149d3548f79869a9a20ccfcbcf7
JonyEpsilon <github@j-star.org>
2014-04-04 16:10:37 +0100

    Get something decent looking together.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :plugins [[lein-gorilla "0.2.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [expresso "0.2.0"]]
+  :plugins [[lein-gorilla "0.3.0-SNAPSHOT"]])
--- src/sr_basics/render.clj
+++ src/sr_basics/render.clj
@@ -6,11 +6,13 @@
   [expr]
   (if (seq? expr)
     (case (first expr)
-      +  (str/join " + " (map latexify (rest expr)))
-      -  (str/join " - " (map latexify (rest expr)))
+      +  (str "(" (str/join " + " (map latexify (rest expr))) ")")
+      -  (str "(" (str/join " - " (map latexify (rest expr))) ")")
       *  (str/join " \\cdot " (map latexify (rest expr)))
-      pdiv (str "\\frac{" (latexify (first (rest expr))) " }{ " (latexify (second (rest expr))) "}"))
-    (if (number? expr) (format "%.3f" expr) (pr-str expr))))
+      pdiv (str "\\frac{" (latexify (first (rest expr))) " }{ " (latexify (second (rest expr))) "}")
+      / (str "\\frac{" (latexify (first (rest expr))) " }{ " (latexify (second (rest expr))) "}")
+      ** (str "(" (latexify (first (rest expr))) ")^{ " (latexify (second (rest expr))) "}"))
+    (if (float? expr) (format "%.3f" expr) (pr-str expr))))
 
 (defrecord ExprLatexView [expr])
 
