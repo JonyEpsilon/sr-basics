cfecc001c3289a56cf1029051997c7eefdbca308
JoelWRussell <jwr111@ic.ac.uk>
2015-11-11 11:29:34 +0000

    Added algebolic dependency

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [darwin "1.0.0"]
-                 [expresso "0.2.0"]]
+                 [expresso "0.2.0"]
+                 [algebolic "1.0.0"]]
   :plugins [[lein-gorilla "0.3.4"]])
